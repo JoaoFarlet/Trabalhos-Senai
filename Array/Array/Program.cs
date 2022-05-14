f87d56176edf6cdc790d7df0cc48f9d4c03624d2
JoaoFarlet <105008701+JoaoFarlet@users.noreply.github.com>
2022-05-14 19:32:00 -0300

    Add files via upload

---
+++ Array/Array/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("###programa arrays###");
+            int[] Numeros = new int[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+            Console.WriteLine(Numeros[9]);
+            Console.WriteLine(Numeros[8]);
+            Console.WriteLine(Numeros[7]);
+            Console.WriteLine(Numeros[6]);
+            Console.WriteLine(Numeros[5]);
+            Console.WriteLine(Numeros[4]);
+            Console.WriteLine(Numeros[3]);
+            Console.WriteLine(Numeros[2]);
+            Console.WriteLine(Numeros[1]);
+            Console.WriteLine(Numeros[0]);
+        }
+    }
+}
+
