4c39200444839b352b8e767d43fd8d2b0a533c66
JoaoFarlet <105008701+JoaoFarlet@users.noreply.github.com>
2022-05-15 18:20:58 -0300

    trabalhos dia 10.5.2022

---
+++ Cadastro de Funcionários (listas & construtores)/Atividade de Listas e Construtor/Funcionaris.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Atividade_de_Listas_e_Construtor
+{
+    class Funcionaris
+    {
+        public string Nomis;
+        public int Idadis;
+        public string Emailis;
+        public string Departamentis;
+        public int Salaris;
+
+
+        public Funcionaris(string nome, int salario, int idade, string email, string departamento)
+        {
+            Nomis = nome;
+            Salaris = salario;
+            Idadis = idade;
+            Emailis = email;
+            Departamentis = departamento;
+        }
+
+        public override string ToString()
+        {
+            return "Nome: " + Nomis
+                + "\nIdade: " + Idadis
+                + "\nEmai: " + Emailis
+                + "\nDepartamento: " + Departamentis
+                + "\nSalario: " + Salaris;
+        }
+    }
+}
---
+++ Cadastro de Funcionários (listas & construtores)/Atividade de Listas e Construtor/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Atividade_de_Listas_e_Construtor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Funcionaris> Trabalhadoris = new List<Funcionaris>();
+
+            string opcao;
+
+            do
+            {
+                Console.WriteLine("###Programa para Cadastro de novos Funcinarios###  \n");
+                Console.Write("Insira o nome do funcionario: ");
+                string nome = Console.ReadLine();
+
+                Console.Write("Insira o salario do funcionario: ");
+                int salario = int.Parse(Console.ReadLine());
+
+                Console.Write("Insira a idade do funcionario: ");
+                int idade = int.Parse(Console.ReadLine());
+
+                Console.Write("Insira o  com o e-mail do funcionario: ");
+                string email = Console.ReadLine();
+
+                Console.Write("Insira o  o departamento do funcionario: ");
+                string departamento = Console.ReadLine();
+                departamento.ToLower();
+
+                Trabalhadoris.Add(new Funcionaris(nome, salario, idade, email, departamento));
+
+                Console.Write("Deseja cadastrar novo funcionario?");
+                opcao = (Console.ReadLine());
+            } while (opcao == "S" || opcao == "s");
+
+            Console.WriteLine("###################################################################");
+
+            foreach (Funcionaris obj in Trabalhadoris)
+            {
+                Console.WriteLine(obj);
+            }
+
+            Console.WriteLine("###################################################################");
+
+            int contadorRh = 0;
+            if (Trabalhadoris.Exists(x => x.Departamentis == "rh"))
+            {
+                foreach (Funcionaris obj in Trabalhadoris)
+                {
+                    contadorRh++;
+                }
+                Console.WriteLine("Rh:"+contadorRh);
+            }
+
+            int contadorDesing = 0;
+            if (Trabalhadoris.Exists(x => x.Departamentis == "desing"))
+            {
+                foreach (Funcionaris obj in Trabalhadoris)
+                {
+                    contadorDesing++;
+                }
+                Console.WriteLine("Desing:"+contadorDesing);
+            }
+
+
+            int contadorTI = 0;
+            if (Trabalhadoris.Exists(x => x.Departamentis == "ti"))
+            {
+                foreach (Funcionaris obj in Trabalhadoris)
+                {
+                    contadorTI++;
+                }
+                Console.WriteLine("TI:"+contadorTI);
+            }
+        }
+    }
+}
+
