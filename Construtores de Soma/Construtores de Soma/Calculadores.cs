4d37f8d6e5bd8703cbd7682a11ca8f4f4d383bc7
JoaoFarlet <105008701+JoaoFarlet@users.noreply.github.com>
2022-05-24 22:54:59 -0300

    Add files via upload

---
+++ Construtores de Soma/Construtores de Soma/Calculadores.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Construtores_de_Soma
+{
+    class Calculadores
+    {
+        
+        public double X;
+        public double Y;
+        public double A;
+        public double C;
+
+
+        public Calculadores()
+        {
+            
+        }
+        public Calculadores(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public Calculadores(int x, int y, int c) : this(x, y)
+        {
+            C = c;
+        }
+        
+
+        public double Somartor()
+        {
+            return X + Y + A + C;
+        }
+
+        public Calculadores(double x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public double Somator2(double x, int y)
+        {
+            X = x;
+            Y = y;
+            return X + Y;
+        }
+    }
+}
---
+++ Construtores de Soma/Construtores de Soma/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Construtores_de_Soma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculadores calc = new Calculadores() { A = 13.50, C = 50.1 };
+
+            Console.WriteLine(calc.Somartor());
+
+        }
+    }
+}
+
