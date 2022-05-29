08f3e0ceec0dc7b5e97722a036234e2cd8bc90ab
JoaoFarlet <105008701+JoaoFarlet@users.noreply.github.com>
2022-05-29 14:33:03 -0300

    Add files via upload

---
+++ banco Encapsulamento/banco Encapsulamento/Conta.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace banco_Encapsulamento
+{
+    class Conta
+    {
+        public string Nome { get; set; }
+        public int NumerCont { get; set; }
+        public int Saldo { get; private set; }
+
+
+        public Conta(string nome, int numero, int saldo)
+        {
+            Nome = nome;
+            NumerCont = numero;
+        }
+
+        public Conta()
+        {
+        }
+
+        public void Sacar(double valor)
+        {
+            valor -= Saldo;
+        }
+
+        public void deposit(double valor)
+        {
+            valor += Saldo;
+        }
+
+
+
+    }
+}
---
+++ banco Encapsulamento/banco Encapsulamento/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace banco_Encapsulamento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Conta conta = new Conta();
+
+
+            Console.WriteLine("Insira o nome do propietário da conta:");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Insira o numero da conta:");
+            string Numeroconta = Console.ReadLine();
+
+            Console.WriteLine("Gostaria de efetuar alguma operação? D/S");
+            
+
+            string op = Console.ReadLine();
+
+            string OP = op.ToLower();
+
+            double valor;
+
+            if (OP == "d")
+            {
+                do
+                {
+                    Console.WriteLine("Insira o valor do Deposito:");
+                    valor = double.Parse(Console.ReadLine());
+                } while (valor == 0 || valor < 0);
+                
+                conta.deposit(valor);
+            }
+
+            else if (OP == "s")
+            {
+                do
+                {
+                    Console.WriteLine("Insira o valor do Saque:");
+                 valor = double.Parse(Console.ReadLine());
+                conta.Sacar(valor);
+                } while (conta.Saldo == 0 || conta.Saldo < 0);
+            }
+
+            Console.WriteLine("As informações inseridas foram:");
+
+
+            Console.WriteLine(conta.Nome);
+            Console.WriteLine(conta.Saldo);
+            Console.WriteLine(conta.NumerCont);
+
+
+
+        }
+    }
+}
