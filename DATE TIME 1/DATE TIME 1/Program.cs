88e1cfca0cf7afc72802568d2d831fe93f950ab0
JoaoFarlet <105008701+JoaoFarlet@users.noreply.github.com>
2022-06-05 12:01:00 -0300

    Add files via upload

---
+++ DATE TIME 1/DATE TIME 1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DATE_TIME_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insira sua data de nascimento: (aaaa/mm/dd)");
+            var nascimento = DateTime.Parse(Console.ReadLine());
+            var atual = DateTime.Now;
+            var idade = nascimento - atual;
+
+            Console.WriteLine("Sua idade é de " + idade);
+        }
+    }
+}
